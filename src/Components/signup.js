29e654b80fecda6102692cf5877d1f8a92a15b29
braQobiGH <64052798+braQobiGH@users.noreply.github.com>
2020-06-15 07:50:08 -0700

    carousel added

--- src/Components/signup.js
+++ src/Components/signup.js
@@ -1,16 +1,66 @@
 import React from "react";
 import "../Css/signup.css";
 import { Form, Button, Col, Row } from "react-bootstrap";
-// import logo from "../images/mylogo.jpg"
-// import happy from "../images/happy.jpg"
-// import join from "../images/join.jpg"
-// import meeting from "../images/meeting.jpg"
-// import walk from "../images/walk.jpg"
-// import Carousel from 'react-bootstrap/Carousel'
+ import logo from "../images/mylogo.jpg"
+ import happy from "../images/happy.jpg"
+ import join from "../images/join.jpg"
+ import meeting from "../images/meeting.jpg"
+ import walk from "../images/walk.jpg"
+ import Carousel from 'react-bootstrap/Carousel'
 
 export default (props) => (
   <div className="container-div">
-    <div className='carousel-div'></div>
+    <div className='carousel-div'>
+
+    <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={meeting} style={{height:"370px",width:"100%"}}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={walk} style={{height:"370px",width:"100%"}}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={join} style={{height:"370px",width:"100%"}}
+      alt="Third slide"
+    />
+      <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={happy} style={{height:"370px",width:"100%"}}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    </div>
     <div  className="signup-form-div">
       <center >
     <h4>Create Account For Free</h4>
@@ -35,7 +85,7 @@ export default (props) => (
         <Form.Label>Password</Form.Label>
         <Form.Control type="number" placeholder="Mobile No." required />
       </Form.Group>
-      <Button  type="submit" className="col-12 btn pulse" style={{backgroundColor:"darkorange"}}>
+      <Button  type="submit" variant="primary" className="col-12 btn pulse" >
         Sign Up
       </Button>
       </Form>
