8a9114b89015f3a0d01740f64f1878ab1caba1e0
ashish19977 <48292897+ashish19977@users.noreply.github.com>
2020-06-15 21:46:03 +0530

    some changes in css by me

--- src/Components/signup.js
+++ src/Components/signup.js
@@ -16,23 +16,23 @@ export default (props) => (
     <h4>Create Account For Free</h4>
     </center>
     <Form style={{width:'100%'}}>
-      <Form.Group controlId="formBasicEmail">
+      <Form.Group controlId="formBasicEmail2">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" required />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group controlId="formBasicPassword2">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" required />
       </Form.Group>
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group controlId="formBasicPassword2a">
         <Form.Label>Confirm Password</Form.Label>
         <Form.Control type="password" placeholder="Password" required />
       </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
+      <Form.Group controlId="formBasicMobile">
+        <Form.Label>Mobile No.</Form.Label>
         <Form.Control type="number" placeholder="Mobile No." required />
       </Form.Group>
       <Button  type="submit" className="col-12 btn pulse" style={{backgroundColor:"darkorange"}}>
