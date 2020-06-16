a4f89b8b972f86fc84923efa31ec51878fd539d5
fleczy200 <42758135+fleczy200@users.noreply.github.com>
2020-06-15 22:46:42 -0700

    commit master

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,11 +3,13 @@ import "../Css/signup.css";
 import { Form, Container,Button, Col, Row } from "react-bootstrap";
 
 export default(props) =>{
+    return(
     <div className ="Container">
         <div className ="Row">
             <div className="col-sm-12">
-                
+
             </div>
         </div>
     </div>
+    )
 }
